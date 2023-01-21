b8a418b638537dc31506a551dfeaf2dc3eb3b626
wang_siyuan <darknightghost@gmail.com>
2023-01-21 22:02:19 +0800

    UTC:2023-01-21 14:02:19 : result test

--- test/source/result/reference_test.cc
+++ test/source/result/reference_test.cc
@@ -41,4 +41,40 @@ TEST(Result, reference)
         ASSERT_TRUE(
             ::std::is_rvalue_reference<decltype(value.value<int &&>())>::value);
     }
+
+    {
+        int                                     a = 100;
+        ::remotePortMapper::Result<void, int &> value;
+        ASSERT_DEATH(value.value<int &>(), ".*");
+        ASSERT_FALSE(value.good());
+        ASSERT_FALSE(value.ok());
+        ASSERT_FALSE(value);
+
+        value = value.makeError(a);
+        ASSERT_TRUE(value.good());
+        ASSERT_FALSE(value.ok());
+        ASSERT_FALSE(value);
+        ASSERT_EQ(value.value<int &>(), 100);
+        ASSERT_TRUE(
+            ::std::is_lvalue_reference<decltype(value.value<int &>())>::value);
+
+        value.value<int &>() = 200;
+        ASSERT_EQ(a, 200);
+    }
+    {
+        int                                      a = 100;
+        ::remotePortMapper::Result<void, int &&> value;
+        ASSERT_DEATH(value.value<int &&>(), ".*");
+        ASSERT_FALSE(value.good());
+        ASSERT_FALSE(value.ok());
+        ASSERT_FALSE(value);
+
+        value = value.makeError(::std::move(a));
+        ASSERT_TRUE(value.good());
+        ASSERT_FALSE(value.ok());
+        ASSERT_FALSE(value);
+        ASSERT_EQ(value.value<int &&>(), 100);
+        ASSERT_TRUE(
+            ::std::is_rvalue_reference<decltype(value.value<int &&>())>::value);
+    }
 }
